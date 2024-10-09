f5da7aba4e3ef28a260209e2909809f7e4f0c1bb
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-10-09 14:13:37 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 # Mount the storybook-static directory to serve static files
-app.mount("/", StaticFiles(directory="storybook-static"), name="storybook")
+app.mount("/", StaticFiles(directory="/app/storybook-static"), name="storybook")
