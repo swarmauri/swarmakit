cbd27e09d39a5e11fc85e9787b8404e920941736
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-10-12 15:38:12 -0500

    reorg + add vue-ts + add sveltekit

--- app/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-
-# Mount the storybook-static directory to serve static files
-app.mount("/", StaticFiles(directory="/app/storybook-static"), name="storybook")
