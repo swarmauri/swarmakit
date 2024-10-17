b1cfa082e242fb21c1f74231bd8e5c52bdffc4cc
cobycloud <25079070+cobycloud@users.noreply.github.com>
2024-10-17 10:16:47 -0500

    Update AdminViewScheduler.stories.ts

--- libs/vue/src/components/AdminViewScheduler/AdminViewScheduler.stories.ts
+++ libs/vue/src/components/AdminViewScheduler/AdminViewScheduler.stories.ts
@@ -1,6 +1,13 @@
 import { Meta, StoryFn } from '@storybook/vue3';
 import AdminViewScheduler from './AdminViewScheduler.vue';
 
+// Define an Event type (this can be shared from the component)
+interface Event {
+  id: number;
+  title: string;
+  date: string;
+}
+
 export default {
   title: 'component/Scheduling/AdminViewScheduler',
   component: AdminViewScheduler,
@@ -21,15 +28,15 @@ Default.args = {};
 export const EventAdded = Template.bind({});
 EventAdded.args = {
   feedbackMessage: 'Event added successfully!',
-  addNewEvent: () => {
-    console.log('Custom addNewEvent function called');
+  addNewEvent: (event: Event) => {
+    console.log('Custom addNewEvent function called', event);
   },
 };
 
 export const EventEdited = Template.bind({});
 EventEdited.args = {
   feedbackMessage: 'Event "Team Meeting" edited successfully!',
-  editEvent: (event) => {
+  editEvent: (event: Event) => {
     console.log(`Custom editEvent function called for event: ${event.title}`);
   },
 };
@@ -37,7 +44,7 @@ EventEdited.args = {
 export const EventDeleted = Template.bind({});
 EventDeleted.args = {
   feedbackMessage: 'Event deleted successfully!',
-  deleteEvent: (eventId) => {
+  deleteEvent: (eventId: number) => {
     console.log(`Custom deleteEvent function called for event ID: ${eventId}`);
   },
 };
