00b5f913847139f2bdfaa81b3f9bc122bd5032d4
Erick559 <erickungu82@gmail.com>
2024-10-22 12:48:31 +0300

    Fix(CardFooter.stories.ts): Resolve the import error for StoryFn. Replaced the `Story` import from storybook with `StoryFn` as thats the required module.
    This fix also resolves the type error for `args` as `StoryFn` uses the components types.

--- libs/vue/src/components/CardFooter/CardFooter.stories.ts
+++ libs/vue/src/components/CardFooter/CardFooter.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, Story } from '@storybook/vue3';
+import { Meta, StoryFn } from '@storybook/vue3';
 import CardFooter from './CardFooter.vue';
 
 export default {
@@ -12,7 +12,7 @@ export default {
   },
 } as Meta;
 
-const Template: Story = (args) => ({
+const Template: StoryFn = (args) => ({
   components: { CardFooter },
   setup() {
     return { args };
