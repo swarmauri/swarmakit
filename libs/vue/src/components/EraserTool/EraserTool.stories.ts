4dc454a80df9090cb7230d87ec0ef210d89b2b97
Erick559 <erickungu82@gmail.com>
2024-10-23 16:40:17 +0300

    fix:(EraserTool.stories.ts) - Fix import error for the StoryFn module.
    - Fixed incorrect import of StoryFn module to ensure proper functionality.

--- libs/vue/src/components/EraserTool/EraserTool.stories.ts
+++ libs/vue/src/components/EraserTool/EraserTool.stories.ts
@@ -1,5 +1,5 @@
 import EraserTool from './EraserTool.vue';
-import { Meta, Story } from '@storybook/vue3';
+import { Meta, StoryFn } from '@storybook/vue3';
 
 export default {
   title: 'component/Drawing/EraserTool',
@@ -7,7 +7,7 @@ export default {
   tags: ['autodocs'],
 } as Meta;
 
-const Template: Story = (args) => ({
+const Template: StoryFn = (args) => ({
   components: { EraserTool },
   setup() {
     return { args };
