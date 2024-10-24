9debab769ff836e7e20674752fca55c6a61e7db8
Erick559 <erickungu82@gmail.com>
2024-10-24 16:41:02 +0300

    fix(PokerChips.stories.ts): Resolve incorrect import for StoryFn.

--- libs/vue/src/components/PokerChips/PokerChips.stories.ts
+++ libs/vue/src/components/PokerChips/PokerChips.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, Story } from '@storybook/vue3';
+import { Meta, StoryFn } from '@storybook/vue3';
 import PokerChips from './PokerChips.vue';
 
 export default {
@@ -7,7 +7,7 @@ export default {
   tags: ['autodocs'],
 } as Meta;
 
-const Template: Story = (args) => ({
+const Template: StoryFn = (args) => ({
   components: { PokerChips },
   setup() {
     return { args };
