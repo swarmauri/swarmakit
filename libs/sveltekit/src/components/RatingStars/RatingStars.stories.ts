ac1ff5d7359d988789a8a8f4b52d3b8be4f2605a
Erick559 <erickungu82@gmail.com>
2024-11-11 08:42:19 +0300

    fix(RatingStarts.stories.ts): Resolve TypeScript error for RatingStars.stories.ts args props
    - Updated the RatingStars story file to correctly import the RatingStars component and define the `argTypes` for the props, allowing Storybook to properly handle the component's properties.
    - This change addresses the TypeScript error: "Object literal may only specify known properties, and 'isOpen' does not exist in type 'Partial<ComponentAnnotations<...>>'" by ensuring that the props are correctly defined and typed in both the component and the story file.
    
    With these updates, RatingStars component can now be used in Storybook without type errors, and the props can be controlled as intended.

--- libs/sveltekit/src/components/RatingStars/RatingStars.stories.ts
+++ libs/sveltekit/src/components/RatingStars/RatingStars.stories.ts
@@ -1,5 +1,5 @@
 import RatingStars from './RatingStars.svelte';
-import type { Meta, StoryObj } from '@storybook/svelte';
+import type { Meta, StoryFn } from '@storybook/svelte';
 
 const meta: Meta<RatingStars> = {
   title: 'component/Indicators/RatingStars',
@@ -24,32 +24,31 @@ const meta: Meta<RatingStars> = {
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
+const Template:StoryFn<RatingStars> = (args) => ({
+  Component:RatingStars,
+  props:args,
+}); 
 
-export const Default: Story = {
-  args: {
-    rating: 3,
-    state: 'inactive',
-  }
+export const Default = Template.bind({});
+Default.args = {
+  rating:3,
+  state:'inactive',
 };
 
-export const Hover: Story = {
-  args: {
-    rating: 3,
-    state: 'hover',
-  }
+export const Hover = Template.bind({});
+Hover.args = {
+  rating:3,
+  state:'hover',
 };
 
-export const Selected: Story = {
-  args: {
-    rating: 4,
-    state: 'selected',
-  }
+export const Selected = Template.bind({});
+Selected.args = {
+  rating:4,
+  state:'selected',
 };
 
-export const Inactive: Story = {
-  args: {
-    rating: 0,
-    state: 'inactive',
-  }
+export const Inactive = Template.bind({});
+Inactive.args = {
+  rating:0,
+  state:'inactive',
 };
