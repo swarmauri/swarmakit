ebb707af312376cda1fdea2d16aa30d19f4dcdda
Erick559 <erickungu82@gmail.com>
2025-01-06 11:33:51 +0300

    Refactor: Update ActionSheet story to use StoryFn type for better TypeScript support
    - Changed the story template type from Story to StoryFn for improved compatibility with TypeScript.
    - Imported ActionSheet to enhance type safety in the story definition.

--- libs/react/src/components/ActionSheet/ActionSheet.stories.tsx
+++ libs/react/src/components/ActionSheet/ActionSheet.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Meta, Story } from '@storybook/react';
+import { Meta, StoryFn } from '@storybook/react';
 import ActionSheet, { ActionSheetProps } from './ActionSheet';
 
 export default {
@@ -8,7 +8,7 @@ export default {
   tags: ['autodocs'],
 } as Meta;
 
-const Template: Story<ActionSheetProps> = (args) => <ActionSheet {...args} />;
+const Template: StoryFn<ActionSheetProps> = (args) => <ActionSheet {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
