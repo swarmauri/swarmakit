20901c1eac1832c0e382aef6fbf3d40400d0011c
Erick559 <erickungu82@gmail.com>
2025-01-06 13:15:39 +0300

    Refactor: Update Avatar story to use StoryFn type for better TypeScript support
    - Changed the story template type from Story to StoryFn for improved compatibility with TypeScript.

--- libs/react/src/components/Avatar/Avatar.stories.tsx
+++ libs/react/src/components/Avatar/Avatar.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Meta, Story } from '@storybook/react';
+import { Meta, StoryFn } from '@storybook/react';
 import Avatar, { AvatarProps } from './Avatar';
 
 export default {
@@ -8,7 +8,7 @@ export default {
   tags: ['autodocs'],
 } as Meta;
 
-const Template: Story<AvatarProps> = (args) => <Avatar {...args} />;
+const Template: StoryFn<AvatarProps> = (args) => <Avatar {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
