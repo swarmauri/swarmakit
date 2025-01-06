73eb6793dc3c1ab9af88a14e7f41fccc4ffe05e9
Erick559 <erickungu82@gmail.com>
2025-01-06 12:50:13 +0300

    Refactor: Update AreaChart story to use StoryFn type for better TypeScript support
    - Changed the story template type from Story to StoryFn for improved compatibility with TypeScript.

--- libs/react/src/components/AreaChart/AreaChart.stories.tsx
+++ libs/react/src/components/AreaChart/AreaChart.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Meta, Story } from '@storybook/react';
+import { Meta, StoryFn } from '@storybook/react';
 import AreaChart, { AreaChartProps } from './AreaChart';
 
 export default {
@@ -8,7 +8,7 @@ export default {
   tags: ['autodocs'],
 } as Meta;
 
-const Template: Story<AreaChartProps> = (args) => <AreaChart {...args} />;
+const Template: StoryFn<AreaChartProps> = (args) => <AreaChart {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
